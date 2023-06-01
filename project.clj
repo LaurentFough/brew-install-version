c6ef5edbcb517e3f6d92b958d35a820874472280
Łaurent ʘ❢Ŧ Ŧough <LaurentFough@users.noreply.github.com>
2023-06-01 11:25:04 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject brew-install-specific "0.1.0"
+(defproject brew-install-version "0.1.0"
   :description "Install specific versions of brew packages."
-  :url "https://github.com/divs1210/brew-install-specific"
+  :url "https://github.com/LaurentFough/brew-install-version"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clansi "1.0.0"]
                  [clj-jgit "1.0.0-beta3"]
                  [ch.qos.logback/logback-classic "1.2.3"]]
-  :main ^:skip-aot brew-install-specific.core
+  :main ^:skip-aot brew-install-version.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
